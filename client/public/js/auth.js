e174e363cc1a43e144a8056c5339f60b833c2707
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2025-01-16 20:01:41 +0000

    function loginUsuario

---
+++ client/public/js/auth.js
@@ -0,0 +1,6 @@
+document.getElementById("loginButton").onclick = loginUsuario;
+
+function loginUsuario() {
+    console.log("Login bem-sucedido!");
+    window.location.href = "dashboard2.html"; // Substitua pelo URL real do redirecionamento
+  }
