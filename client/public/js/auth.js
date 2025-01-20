8463ff3f512e8d9e43516f1c12fbdda9d72219d8
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2025-01-20 22:39:04 +0000

    conexão ao firebase

--- client/public/js/auth.js
+++ client/public/js/auth.js
@@ -1,6 +1,26 @@
-document.getElementById("loginButton").onclick = loginUsuario;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app-compat.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth-compat.js";
 
-function loginUsuario() {
-    console.log("Login bem-sucedido!");
-    window.location.href = "dashboard2.html"; // Substitua pelo URL real do redirecionamento
-  }
+const firebaseConfig = {
+    apiKey: "AIzaSyAeC9_22GqZt-s80gW0xcWLyguhEWvdnw4",
+    authDomain: "sdge-f4d9d.firebaseapp.com",
+    projectId: "sdge-f4d9d",
+    storageBucket: "sdge-f4d9d.firebasestorage.app",
+    messagingSenderId: "77651645333",
+    appId: "1:77651645333:web:6911a17f0de1d048b99c9c",
+    measurementId: "G-7BWQ9BPNZT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+console.log('antes');
+signInWithEmailAndPassword(auth, "igoramarilha23@gmail.com", "amarilha23")
+    .then(response => {
+        console.log('Login realizado com sucesso:', response.user);
+    })
+    .catch(error => {
+        console.error('Erro ao fazer login:', error.message);
+    });
+console.log('depois');
